ee3dcce1f89fce1170aefef3f9020c1cbb372d37
daniel22s <cruzdani543@gmail.com>
2026-01-18 20:32:00 -0500

    Subir a produccion

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bvtt++a52)e!qm52*jk^golh^an5s*2yqm5)a@w)-2e_*&+qos
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,3 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
